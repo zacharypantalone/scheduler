eeb98ec13e863235e1eadc1a72fa7ad030b144f6
Zachary Pantalone <zachary.pantalone@gmail.com>
2022-09-19 17:47:09 -0400

    oru first appointment views

--- stories/index.js
+++ stories/index.js
@@ -9,6 +9,7 @@ import InterviewerListItem from "components/InterviewerListItem";
 import InterviewerList from "components/InterviewerList";
 import Appointment from "components/Appointment/index.js";
 import Header from "components/Appointment/Header.js";
+import Empty from "components/Appointment/Empty.js";
 
 
 
@@ -143,3 +144,4 @@ storiesOf("Button", module)
         .add("Appointment", () => <Appointment />)
         .add("Appointment with Time", () => <Appointment time="12pm" />)
         .add("Header", () => <Header time="12pm" />)
+        .add("Empty", () => <Empty onAdd={action("onAdd")}/>)
