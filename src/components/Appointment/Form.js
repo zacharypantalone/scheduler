2465b5eb759efbd64158c942fd78793052b3bc43
Zachary Pantalone <zachary.pantalone@gmail.com>
2022-09-19 20:29:22 -0400

    add state for student

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -1,13 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import Button from "components/Button";
 import InterviewerList from "components/InterviewerList";
 
 export default function Form(props) {
+  const [student, setStudent] = useState(props.student || "");
+  const [interviewer, setInterviewer] = useState(props.interviewer || null);
+
   return (
     <main className="appointment__card appointment__card--create">
   <section className="appointment__card-left">
     <form autoComplete="off">
       <input
+        value={student}
+        onChange={(event) => setStudent(event.target.value)}
         className="appointment__create-input text--semi-bold"
         name="name"
         type="text"
