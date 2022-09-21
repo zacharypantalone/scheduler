90f8ad9a49a545d10abd3e3a83d7f7de6e117afb
Zachary Pantalone <zachary.pantalone@gmail.com>
2022-09-21 19:59:09 -0400

    transforming data with selectors

---
+++ src/helpers/selectors.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+
+export function getAppointmentsForDay (state, dayName) {
+  const actualDay = state.days.find(day => day.name === dayName);
+  if (actualDay) {
+  return actualDay.appointments.map((id) => state.appointments[id]);
+  } else {return []}
+}
