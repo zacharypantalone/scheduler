6f6354746d7ed12fa91d2df26715d0cdedeed139
Zachary Pantalone <zachary.pantalone@gmail.com>
2022-09-22 17:17:00 -0400

    Managing state

--- src/components/Application.js
+++ src/components/Application.js
@@ -50,8 +50,17 @@ const appointments = {
 
 
 export default function Application(props) {
-  const [day, setDay] = useState("Monday");
-  const [days, setDays] = useState([]);
+
+const setDay = day => setState({ ...state, day});
+const setDays = (days) => setState(prev => ({...prev, days}));
+
+  
+
+  const [state, setState] = useState({
+    day: "Monday",
+    days: [],
+    appointments: {}
+  });
 
 useEffect(() => {
   const daysURL = `http://localhost:8001/api/days`;
@@ -60,6 +69,8 @@ useEffect(() => {
 
   })
 },[]);
+
+
 
   return (
     <main className="layout">
@@ -72,8 +83,8 @@ useEffect(() => {
         <hr className="sidebar__separator sidebar--centered" />
         <nav className="sidebar__menu">
           <DayList
-            days={days}
-            value={day}
+            days={state.days}
+            value={state.day}
             onChange={setDay}
             
           />
