5fe44eed6871066c2b2a2140579d9c60ab7e5ae2
Zachary Pantalone <zachary.pantalone@gmail.com>
2022-09-27 17:08:29 -0400

    back function working

--- src/hooks/__tests__/useVisualMode.test.js
+++ src/hooks/__tests__/useVisualMode.test.js
@@ -3,9 +3,21 @@ import useVisualMode from "hooks/useVisualMode";
 
 
 const FIRST = "FIRST";
+const SECOND = "SECOND";
+const THIRD = "THIRD";
 
 test("useVisualMode should initialize with default value", () => {
   const { result } = renderHook(() => useVisualMode(FIRST));
+  
 
+  act(() => result.current.transition(SECOND));
+  expect(result.current.mode).toBe(SECOND);
+
+  act(() => result.current.transition(THIRD, true));
+  expect(result.current.mode).toBe(THIRD);
+
+  act(() => result.current.back());
   expect(result.current.mode).toBe(FIRST);
+  
+
 });
--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -2,6 +2,25 @@ import React, { useState } from "react";
 
 export default function useVisualMode(initial) {
   const [mode, setMode] = useState(initial);
+  const [history, setHistory] = useState([initial]);
 
-  return { mode };
+  console.log("this is mode", mode, "this is history", history);
+
+  function transition(newMode, replace = false) {
+    setMode(newMode);
+
+    if (replace) setHistory((prev) => [...prev, newMode]);
+  }
+
+  function back() {
+    if (history.length >= 1) {
+      setHistory((prev) => {
+        const newHistory = [...prev.slice(0, -1)]
+        setMode(newHistory[newHistory.length -1])
+        return newHistory;
+      });
+    }
+  }
+
+  return { mode, transition, back };
 }
