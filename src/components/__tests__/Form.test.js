5b4198f685805988f9497867c7b713aef51fa26c
Zachary Pantalone <zachary.pantalone@gmail.com>
2022-10-02 18:48:01 -0400

    saving for the day

--- src/components/__tests__/Form.test.js
+++ src/components/__tests__/Form.test.js
@@ -21,7 +21,8 @@ describe("Form", () => {
   it("renders without student name if not provided", () => {
     const { getByPlaceholderText } = render(
       <Form interviewers={interviewers} />
-    );
+    )
+    
     expect(getByPlaceholderText("Enter Student Name")).toHaveValue("");
   });
 
