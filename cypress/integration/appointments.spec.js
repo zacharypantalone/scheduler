a65900af3bfc81b5722e9e66507c4fb5532ce848
Zachary Pantalone <zachary.pantalone@gmail.com>
2022-10-06 17:38:45 -0400

    should edit an interview complete

--- cypress/integration/appointments.spec.js
+++ cypress/integration/appointments.spec.js
@@ -17,11 +17,28 @@ describe("Appointments", () => {
     cy.get("[alt='Sylvia Palmer']").click();
 
     cy.contains("Save").click();
-    
+
     cy.contains(".appointment__card--show", "Lydia Miller-Jones");
     cy.contains(".appointment__card--show", "Sylvia Palmer");
+
   });
 
+  it("should edit an interview", () => {
+    cy.get("[alt=Edit]")
+    .first()
+    .click({ force: true });
+
+    cy.get("[data-testid=student-name-input]").clear().type("Lydia Miller-Jones");
+    cy.get("[alt='Tori Malcolm']").click();
+
+    cy.contains("Save").click();
+
+    cy.contains(".appointment__card--show", "Lydia Miller-Jones");
+    cy.contains(".appointment__card--show", "Tori Malcolm");
+
+  });
+
+  
   
 
 })
